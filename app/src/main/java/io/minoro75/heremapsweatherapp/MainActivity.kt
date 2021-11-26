4992664e96b34ef858e18ffc9be204f4727fb309
minoro75 <minoro75@gmail.com>
2021-11-26 18:56:52 +0200

    setup view binding

--- app/src/main/java/io/minoro75/heremapsweatherapp/MainActivity.kt
+++ app/src/main/java/io/minoro75/heremapsweatherapp/MainActivity.kt
@@ -3,14 +3,15 @@ package io.minoro75.heremapsweatherapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import io.minoro75.heremapsweatherapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding: ActivityMainBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
     }
 
     override fun onSupportNavigateUp(): Boolean {
