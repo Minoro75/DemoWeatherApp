1d6521d43902562a769e5c7e22f0744ab54b6fa4
minoro75 <minoro75@gmail.com>
2021-11-26 18:30:49 +0200

    add navigation

--- app/src/main/java/io/minoro75/heremapsweatherapp/MainActivity.kt
+++ app/src/main/java/io/minoro75/heremapsweatherapp/MainActivity.kt
@@ -2,17 +2,18 @@ package io.minoro75.heremapsweatherapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.minoro75.heremapsweatherapp.cityWeatherFragment.CityWeatherFragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, CityWeatherFragment.newInstance())
-                .commitNow()
-        }
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
