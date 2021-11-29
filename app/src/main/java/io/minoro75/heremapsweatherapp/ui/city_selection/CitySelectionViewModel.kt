7325c46fa65dd15409f97085e2fc620a7a1d02f6
minoro75 <minoro75@gmail.com>
2021-11-29 18:14:05 +0200

    - setup permissions - revieve location updates

--- app/src/main/java/io/minoro75/heremapsweatherapp/ui/city_selection/CitySelectionViewModel.kt
+++ app/src/main/java/io/minoro75/heremapsweatherapp/ui/city_selection/CitySelectionViewModel.kt
@@ -1,5 +1,7 @@
 package io.minoro75.heremapsweatherapp.ui.city_selection
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.minoro75.heremapsweatherapp.repository.WeatherRepository
@@ -9,5 +11,15 @@ import javax.inject.Inject
 class CitySelectionViewModel @Inject constructor(
     private val weatherRepository: WeatherRepository
 ) : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _lat = MutableLiveData<Double>()
+    val lat: LiveData<Double> = _lat
+
+    private val _lon = MutableLiveData<Double>()
+    val lon: LiveData<Double> = _lon
+
+    private val _city = MutableLiveData<String>()
+    val city: LiveData<String> = _city
+
+    fun getCityNameFromCoordinates(lat: Double, lon: Double) {
+    }
 }
